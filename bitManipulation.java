1809bf1958f6932ac685aa141d372f6fc5cd6ab6
SAHIL RAJA <sraza1692@gmail.com>
2025-08-29 18:59:33 +0530

    Add files via upload

---
+++ bitManipulation.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class bitManipulation {
+    public static int getithBit(int n, int i){
+        int bitMask = 1<<i;
+
+        if((n & bitMask) == 0){
+          return 0;
+        }
+     else{
+        return 1;
+     }
+     
+    }
+    public static void main(String args[]){
+        System.out.println(getithBit(10, 1));
+    }
+}
